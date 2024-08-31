519419fd5f05d3299d34ec4d42c1adc114d4c827
Gasterbuzzer <juliankoberg@hotmail.com>
2024-08-31 20:08:23 +0200

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using MelonLoader;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+using SuperCold;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SuperCold")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SuperCold")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9ef8ae47-aad0-407c-bf23-54231d27aee8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(MainClass), "SuperCold", "1.0.0", "Gasterbuzzer", "https://github.com/Gasterbuzzer/SuperCold/releases/")]
+[assembly: MelonGame("SUPERHOT_Team", "SUPERHOT")]
+[assembly: MelonAuthorColor(ConsoleColor.Magenta)]
+[assembly: MelonPriority(100)]
---
+++ src/MainClass.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using SuperhotAssets;
+using System.Collections;
+using static RootMotion.FinalIK.IKSolver;
+
+namespace SuperCold
+{
+
+    public static class BuildInfo
+    {
+        public const string Name = "SuperCold";
+        public const string Description = "Mod for randomizing weapons on player and enemies.";
+        public const string Author = "Gasterbuzzer";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = "https://github.com/Gasterbuzzer/SuperCold/releases/";
+    }
+
+    public class MainClass : MelonMod
+    {
+        public static Material greyWall;
+        public static Material purpleGun;
+        public static Material blueEnemy;
+
+        // Create Materials for later replacing.
+        public override void OnLateInitializeMelon()
+        {
+            greyWall = new Material(Shader.Find("Standard"));
+            greyWall.color = new Color(0.176f, 0.188f, 0.212f, 1f);
+
+            purpleGun = new Material(Shader.Find("Standard"));
+            purpleGun.color = new Color(0.82f, 0.118f, 0.761f, 1f);
+
+            blueEnemy = new Material(Shader.Find("Standard"));
+
+            // Set the main color of the material to blue
+            blueEnemy.color = new Color(0.1f, 0.1f, 1f, 1f);  // _Color set to blue
+
+            // Set the emission color to blue to simulate a subtle glow
+            blueEnemy.EnableKeyword("_EMISSION");
+            blueEnemy.SetColor("_EmissionColor", new Color(0.3f, 0.3f, 0.6f, 1f));  // _EmissionColor set to blue
+
+            // Set glossiness (similar to _Shininess and _Gloss)
+            blueEnemy.SetFloat("_Glossiness", 0.7f);  // A combination of _Shininess and _Gloss values
+
+            // Specular highlights color approximation (using Standard Shader's PBR model)
+            blueEnemy.SetColor("_SpecColor", new Color(0.5f, 0.5f, 0.5f, 1f));  // _SpecColor / _SuperSpecColor equivalent
+            blueEnemy.SetFloat("_Metallic", 0.5f); // Approximating the specular effect
+
+            // Simulate rim color with emission if needed (not directly supported by Standard Shader)
+            Color rimColor = new Color(0.26f, 0.19f, 0.16f, 0f);
+            blueEnemy.SetColor("_EmissionColor", blueEnemy.GetColor("_EmissionColor") + rimColor * 0.3f);  // Simulate a subtle rim effect
+        }
+
+        // On Game Scene Change, replace material of walls and objects with the following names:
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+
+            MelonLogger.Msg($"New Scene Loaded: {sceneName}.");
+
+            ProcessObjectsWithName("pb", greyWall);
+            ProcessObjectsWithName("prefab", greyWall);
+            ProcessObjectsWithName("model", greyWall);
+            ProcessObjectsWithName("Glock", purpleGun);
+            ProcessObjectsWithName("glock", purpleGun);
+
+            SetSpawnerColorBlue();
+            SetBloodBlue();
+            SetTrailBlue();
+            SetEndTowerBlue();
+
+            // Some Spawner Lights may appear later, so we may use this pointless coroutine for now.
+            // Should be fixed by the SetSpawnerColorBlue function.
+            MelonCoroutines.Start(ChangeSpawnerLights());
+        }
+
+        /// <summary>
+        /// Assings Material to all GameObjects containing the name,
+        /// </summary>
+        /// <param name="namePart"> Name to be contained by the GameObject </param>
+        /// <param name="assignedMaterial"> Material to be assigned </param>
+        public static void ProcessObjectsWithName(string namePart, Material assignedMaterial)
+        {
+            // Find all objects in the scene with the name,.
+            GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+
+            foreach (GameObject obj in allObjects)
+            {
+                if (obj.name.Contains(namePart))
+                {
+                    MeshRenderer renderer = obj.GetComponent<MeshRenderer>();
+                    if (renderer != null)
+                    {
+                        renderer.material = assignedMaterial;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets color of the enemy spawner to blue.
+        /// </summary>
+        public static void SetSpawnerColorBlue()
+        {
+            // Find all objects in the scene, including inactive ones.
+            GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+
+            foreach (GameObject obj in allObjects)
+            {
+                if (obj.name.Contains("FrontSpotZcieniem") || obj.name.Contains("PointLightBezcieniowy") || obj.name.Contains("Point light na przeciwnika"))
+                {
+
+                    Light lightComponent = obj.GetComponent<Light>();
+                    if (lightComponent != null)
+                    {
+                        obj.GetComponent<Light>().color = Color.blue;
+                    }
+                    else
+                    {
+                        MelonLogger.Warning("Could not find light component in enemy spawner.");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets color of the enemy blood to blue.
+        /// </summary>
+        public static void SetBloodBlue()
+        {
+            // Find all objects in the scene, including inactive ones.
+            GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+
+            foreach (GameObject obj in allObjects)
+            {
+                if (obj.name.Contains("Particles - geometry") || obj.name.Contains("Particles - enemyballs"))
+                {
+
+                    ParticleSystem particleComponent = obj.GetComponent<ParticleSystem>();
+                    ParticleSystemRenderer particleRendererComponent = obj.GetComponent<ParticleSystemRenderer>();
+                    if (particleComponent != null)
+                    {
+                        // For now main doesn't work, so we directly use this deprecated way.
+                        particleComponent.startColor = Color.blue;
+                        particleRendererComponent.material = blueEnemy;
+                    }
+                    else
+                    {
+                        MelonLogger.Warning("Could not find particle system component in enemy blood particle.");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets color of the bullet trail to blue.
+        /// </summary>
+        public static void SetTrailBlue()
+        {
+            // Find all objects in the scene, including inactive ones.
+            GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+
+            foreach (GameObject obj in allObjects)
+            {
+                if (obj.name.Contains("Trail(Clone)"))
+                {
+
+                    MeshRenderer bulletTrail = obj.GetComponent<MeshRenderer>();
+                    if (bulletTrail != null)
+                    {
+                        bulletTrail.material.color = Color.blue;
+                    }
+                    else
+                    {
+                        MelonLogger.Warning("Could not find mesh renderer in bullet trail.");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets final tower of the last level to blue.
+        /// </summary>
+        public static void SetEndTowerBlue()
+        {
+            // Find all objects in the scene, including inactive ones.
+            GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+
+            foreach (GameObject obj in allObjects)
+            {
+                if (obj.name.Contains("piramida-krysztalowa-02"))
+                {
+
+                    MeshRenderer bulletTrail = obj.GetComponent<MeshRenderer>();
+                    if (bulletTrail != null)
+                    {
+                        bulletTrail.material = blueEnemy;
+                    }
+                    else
+                    {
+                        MelonLogger.Warning("Could not find mesh renderer in bullet trail.");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// (Deprecated) Changes the enemy spawner lights when they spawn for the first time. Not usefull as we can do it beforehand.
+        /// </summary>
+        public static IEnumerator ChangeSpawnerLights()
+        {
+            GameObject frontSpotZcieniem = null;
+
+            while (frontSpotZcieniem == null)
+            {
+                yield return null; // Wait for the next frame
+                frontSpotZcieniem = GameObject.Find("FrontSpotZcieniem");
+            }
+
+            MelonLogger.Msg("Found Spawner Lights Available.");
+
+            SetSpawnerColorBlue();
+        }
+    }
+
+    [HarmonyPatch(typeof(MaterialDefinition), "InitDictionaries", new Type[] { })]
+    public static class PatchMaterialSet
+    {
+
+        /// <summary>
+        /// Patches Material Defintion to change the enemy material to blue.
+        /// </summary>
+        /// <param name="__originalMethod"> Method which was called (Used to get class type.) </param>
+        /// <param name="__instance"> Caller of function. </param>
+        private static void Postfix(MethodBase __originalMethod, object __instance)
+        {
+            Type classType = __originalMethod.DeclaringType;
+            FieldInfo mat = classType.GetField("Material", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            Material currentMat = (Material)mat.GetValue(__instance);
+
+            if (currentMat.name == "MAIN RedCrystalNuEnemyMaterial")
+            {
+
+                // Create a new material with a default shader
+                Material blueMaterial = new Material(Shader.Find("Standard"));
+
+                blueMaterial.shaderKeywords = currentMat.shaderKeywords;
+                blueMaterial.globalIlluminationFlags = currentMat.globalIlluminationFlags;
+
+                // Set the main color of the material to blue
+                blueMaterial.color = new Color(0.1f, 0.1f, 1f, 1f);  // _Color set to blue
+
+                // Set the emission color to blue to simulate a subtle glow
+                blueMaterial.EnableKeyword("_EMISSION");
+                blueMaterial.SetColor("_EmissionColor", new Color(0.3f, 0.3f, 0.6f, 1f));  // _EmissionColor set to blue
+
+                // Set glossiness (similar to _Shininess and _Gloss)
+                blueMaterial.SetFloat("_Glossiness", 0.7f);  // A combination of _Shininess and _Gloss values
+
+                // Specular highlights color approximation (using Standard Shader's PBR model)
+                blueMaterial.SetColor("_SpecColor", new Color(0.5f, 0.5f, 0.5f, 1f));  // _SpecColor / _SuperSpecColor equivalent
+                blueMaterial.SetFloat("_Metallic", 0.5f); // Approximating the specular effect
+
+                // Simulate rim color with emission if needed (not directly supported by Standard Shader)
+                Color rimColor = new Color(0.26f, 0.19f, 0.16f, 0f);
+                blueMaterial.SetColor("_EmissionColor", blueMaterial.GetColor("_EmissionColor") + rimColor * 0.3f);  // Simulate a subtle rim effect
+
+                mat.SetValue(__instance, blueMaterial);
+            }
+
+        }
+    }
+
+    [HarmonyPatch(typeof(StaticParticles), "Awake", new Type[] { })]
+    public static class PatchEnemySpawnEffectDefault
+    {
+
+        /// <summary>
+        /// Patches the Static Particle class to replace the light colors of Enemy Spawner Particles. Note: I believe this is can be removed.
+        /// </summary>
+        /// <param name="__originalMethod"> Method which was called (Used to get class type.) </param>
+        /// <param name="__instance"> Caller of function. </param>
+        private static void Postfix(MethodBase __originalMethod, object __instance)
+        {
+            Type classType = __originalMethod.DeclaringType;
+
+            GameObject spawnEffectGameObject = ((MonoBehaviour)__instance).gameObject;
+
+            spawnEffectGameObject.transform.Find("FrontSpotZcieniem").GetComponent<Light>().color = Color.blue;
+            spawnEffectGameObject.transform.Find("PointLightBezcieniowy").GetComponent<Light>().color = Color.blue;
+        }
+    }
+}
